fdd54d618e1abe87f859e93c356e690850ab71d9
Nolan Bastien <80637967+nolanbastien@users.noreply.github.com>
2023-08-18 22:29:52 -0400

    Add ImGui

--- OpenGLTest/src/Main.cpp
+++ OpenGLTest/src/Main.cpp
@@ -17,6 +17,9 @@
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw_gl3.h"
+
 void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
@@ -82,10 +85,7 @@ int main(void)
 
     // The projection matrix takes a vertex from the specified boundaries "space" then transforms it to a -1.0 to 1.0 space for OpenGL to render.
     glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f); // This sets the boundaries of our screen. This is a 4:3 aspect ratio
-    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-1.0f, 0.0f, 0.0f));
-    glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(1.0f, 1.0f, 0.0f));
-
-    glm::mat4 mvp = proj * view * model;
+    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
 
     Shader shader("res/shaders/Basic.shader");
     shader.Bind();
@@ -94,7 +94,6 @@ int main(void)
     Texture texture("res/textures/grass_fullres.png");
     texture.Bind(); // Binds texture to a texture slot
     shader.SetUniform1i("u_Texture", 0); // Gets texture from texture slot 0
-    shader.SetUniformMat4f("u_MVP", mvp);
 
     va.Unbind();
     shader.Unbind();
@@ -102,6 +101,12 @@ int main(void)
     ib.Unbind();
 
     Renderer renderer;
+
+    ImGui::CreateContext();
+    ImGui_ImplGlfwGL3_Init(window, true);
+    ImGui::StyleColorsDark();
+
+    glm::vec3 translation(0.0f, 0.0f, 0.0f);
 
     float r = 0.0f;
     float increment = 0.05f;
@@ -114,9 +119,17 @@ int main(void)
         
         renderer.Clear();
 
+        ImGui_ImplGlfwGL3_NewFrame();
+
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
+
+        glm::mat4 mvp = proj * view * model;
+
         // Rendering commands here
         shader.Bind();
         shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f); // Set data in shader (Remember: Uniforms are per draw, don't edit uniforms in between drawings)
+        shader.SetUniformMat4f("u_MVP", mvp);
+
         renderer.Draw(va, ib, shader);
         
         if (r > 1.0f)
@@ -126,12 +139,21 @@ int main(void)
 
         r += increment;
 
+        {
+            ImGui::SliderFloat3("Translation", &translation.x, -2.0f, 2.0f);
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        }
+
+        ImGui::Render();
+        ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
+
         // Swap Buffers
         glfwSwapBuffers(window); // As soon as the rendering commands are finished, the front and back buffers are swapped.
 
         glfwPollEvents(); // Checks if any events are triggered (like keyboard or mouse mouvement).
     }
-
+    ImGui_ImplGlfwGL3_Shutdown();
+    ImGui::DestroyContext();
     glfwTerminate();
     return 0;
 }
